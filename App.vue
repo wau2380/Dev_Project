6fb53692fd66a21ff76c494b2e147101ff4e09db
eunryeong1 <68606265+eunryeong1@users.noreply.github.com>
2021-09-30 22:01:20 +0900

    Update App.vue

--- App.vue
+++ App.vue
@@ -15,7 +15,7 @@
         <li><a class="menus" href="">Logout</a></li>
       </ul>
     </div>
-    <div class="row2"></div>
+    <div class="row3">
     <div id="Card_News">
       <div class="Card_Container">
         <div class="card card-0"></div>
@@ -26,6 +26,7 @@
         <div class="card card-4"></div>
         <div class="card card-5"></div>
       </div>
+    </div>
     </div>
   </body>
 </template>
@@ -82,6 +83,9 @@ button {
   align-items: center;
   background-color: white;
 }
+.row3{
+  overflow: auto;
+}
 @media screen and (max-width: 768) {
   .row2 {
     flex-direction: column;
@@ -135,7 +139,6 @@ li {
   width: 300px;
   border: 1px solid;
 }
-
 .card-1 {
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
@@ -143,23 +146,18 @@ li {
 .card-0:hover {
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
-
 .card-1:hover {
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
-
 .card-2:hover {
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 }
-
 .card-3:hover {
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 }
-
 .card-4:hover {
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
-
 .card-5:hover {
   box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
 }
