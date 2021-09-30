568896dac8484bffe2018fc322a3ef40c8fe7100
wau2380(yijung park) <79182893+wau2380@users.noreply.github.com>
2021-09-30 20:04:39 +0900

    feat : 카드뉴스 추가 (2021-09-30)
    ppt에 나와있는 예정대로 카드뉴스를 틀을 추가했습니다.

--- App.vue
+++ App.vue
@@ -1,134 +1,171 @@
 <template>
   <body>
     <div class="row1">
-      <div  class="search">
-        <input type="text" placeholder="검색어 입력">
+      <div class="search">
+        <input type="text" placeholder="검색어 입력" />
         <button>검색</button>
       </div>
     </div>
 
     <div class="row2">
-       <ul class="top-menu">
-        <li><a class="menus" href="#">home</a></li>
-        <li><a class="menus" href="#">Company</a></li>
-        <li><a class="menus" href="#">Mypage</a></li>
-        <li><a class="menus" href="#">logout</a></li>
-       </ul>
+      <ul class="top-menu">
+        <li><a class="menus" href="">Home</a></li>
+        <li><a class="menus" href="">Company</a></li>
+        <li><a class="menus" href="">Mypage</a></li>
+        <li><a class="menus" href="">Logout</a></li>
+      </ul>
     </div>
-   <div class="row3">
-      
-   </div>
+    <div class="row2"></div>
+    <div id="Card_News">
+      <div class="Card_Container">
+        <div class="card card-0"></div>
+        <div class="card card-1"></div>
+        <div class="card card-2"></div>
+
+        <div class="card card-3"></div>
+        <div class="card card-4"></div>
+        <div class="card card-5"></div>
+      </div>
+    </div>
   </body>
 </template>
 
 <script>
-
 export default {
-  name:"DEV",
-  components: {
-  }
-}
+  name: "DEV",
+  components: {},
+};
 </script>
 
 <style>
-body{
-  width:100%;
+body {
+  width: 100%;
   height: 100%;
-  margin-left:0;
-  margin-right:0;
-
-
+  margin-left: 0;
+  margin-right: 0;
 }
-
-.row1{
-  flex:0.3;
+.row1 {
+  flex: 0.3;
   height: 50px;
-  padding-bottom:5px;
-  border-bottom:3px solid;
+  padding-bottom: 5px;
+  border-bottom: 3px solid;
   justify-content: center;
-
 }
-
-.search{
-  height:40px;
-  width:700px;
-  border:5px solid white;
-  background:whitesmoke;
+.search {
+  height: 40px;
+  width: 700px;
+  border: 5px solid white;
+  background: whitesmoke;
   justify-content: center;
-  margin:auto;
-  border:1px solid;
+  margin: auto;
+  border: 1px solid;
 }
-
-input{
-  font-size:16px;
-  width:630px;
-  padding:10px;
-  border:0px;
-  outline:none;
-  float:left;
+input {
+  font-size: 16px;
+  width: 630px;
+  padding: 10px;
+  border: 0px;
+  outline: none;
+  float: left;
 }
-
-button{
-  width:50px;
-  height:100%;
-  border:0px;
-  outline:none;
-  float:right;
-  
- 
+button {
+  width: 50px;
+  height: 100%;
+  border: 0px;
+  outline: none;
+  float: right;
 }
-
-.row2{
-
-  border-bottom:3px solid;
-  display:flex;
+.row2 {
+  border: 1px solid;
+  display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color:rgb(240, 179, 157);
+  background-color: white;
 }
-
-@media screen and (max-width:768){
-  .row2{
-    flex-direction:column;
+@media screen and (max-width: 768) {
+  .row2 {
+    flex-direction: column;
   }
-  .top-menu{
-    flex-direction:column;
+  .top-menu {
+    flex-direction: column;
   }
-  .li{
-      padding:0px 30px;
-
+  .li {
+    padding: 0px 30px;
   }
-
 }
-
-.top-menu{
-  display:flex;
-  color: white;               
-  float: left;              
-  line-height: 30px;          
-  vertical-align: middle;    
+.top-menu {
+  display: flex;
+  color: white;
+  float: left;
+  line-height: 30px;
+  vertical-align: middle;
   text-align: center;
   justify-content: center;
- 
 }
-ul{
-  list-style-type: none; 
-  float:left; 
+ul {
+  list-style-type: none;
+  float: left;
   justify-content: center;
-  margin:5px auto;
-  padding:0;
+  margin: 5px auto;
+  padding: 0;
 }
-
-
-li{
-  text-decoration:none;
-  color:black;
+li {
+  text-decoration: none;
+  color: black;
   padding-right: 60px;
-  width:150px;
-  font-size:15px;
+  width: 150px;
+  font-size: 15px;
   font-weight: bold;
-  display:inline;
+  display: inline;
+}
+#Card_News {
+  height: 80vh;
+  background: whitesmoke;
+  text-align: center;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.card {
+  background: #fff;
+  border-radius: 4px;
+  display: inline-block;
+  height: 300px;
+  margin: 30px;
+  width: 300px;
+  border: 1px solid;
 }
 
+.card-1 {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.card-0:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
 
+.card-1:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+
+.card-2:hover {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.card-3:hover {
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+}
+
+.card-4:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+
+.card-5:hover {
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+}
+.Card_Container {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: 1fr 1fr 1fr;
+}
 </style>
