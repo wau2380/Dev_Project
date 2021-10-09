c466e81aa67a88a613c892154b779dca01fbe492
Dami Choi <choidami93@gmail.com>
2021-10-09 09:32:28 +0900

    feat : DB 연동 추가
    Firebase 연동

---
+++ src/main/java/newspeed/NewspeedApplication.java
@@ -0,0 +1,13 @@
+package newspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspeedApplication.class, args);
+	}
+
+}
