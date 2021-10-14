ddd675cde2ebb0491d43248fa8d70a109a42e5c1
Dami Choi <choidami93@gmail.com>
2021-10-14 22:27:31 +0900

    refactor : spring boot, vue 소스 통합 작업
    - Spring Boot (Java), Vue 소스 통합 작업
    - main 브랜치에 있는 front-end 소스코드 /vue 디렉토리로 이동 (.gitignore, README.md 파일 제외)
    - /vue 디렉토리에서 npm 구동 가능

---
+++ vue/src/pages/company-page/company-page.js
@@ -0,0 +1,81 @@
+import CompanyPageBar from './components/company-page-bar.vue'
+import CompanyPageSearch from './components/company-page-search.vue'
+import CompanyPageUserList from './components/company-page-user-list.vue'
+import CompanyPageTotalList from './components/company-page-total-list.vue'
+
+export default {
+  components: {
+    CompanyPageSearch,
+    CompanyPageBar,
+    CompanyPageUserList,
+    CompanyPageTotalList,
+  },
+  data() {
+    return {
+      UserCompanys: [
+        { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+        { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+        { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+        { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+        { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+        { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+        { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+        { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+        { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+        { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+        { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+        { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+        { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+        { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+        { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+        { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+      ],
+      companys: [
+        [
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+        ],
+        [
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://media.glassdoor.com/sqll/914956/coupang-squarelogo-1550255639870.png", name: "쿠팡"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+          { logo: "https://blog.kakaocdn.net/dn/OQafi/btqKspax5Yo/HVKIgZo8EQZEbGzrXOfwI1/img.jpg", name: "네이버"},
+          { logo: "https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png", name: "카카오"},
+        ],
+      ]
+    }
+  },
+}
