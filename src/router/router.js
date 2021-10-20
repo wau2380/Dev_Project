437187f3fecee4166ee4731036949b7ff0684e03
ahn0min <ahn0min95@gmail.com>
2021-10-21 03:50:13 +0900

    style: router name변경

--- src/router/router.js
+++ src/router/router.js
@@ -3,27 +3,27 @@ import { createWebHistory, createRouter } from "vue-router";
 const routes = [
   {
     path: "/",
-    name: "Main",
+    name: "MainPage",
     component: () => import("@/pages/main-page/main-page.vue"), // 동적 import
   },
   {
     path: "/Join",
-    name: "Join",
+    name: "JoinPage",
     component: () => import("@/pages/join-page/join-page.vue"),
   },
   {
     path: "/Login",
-    name: "Login",
+    name: "LoginPage",
     component: () => import("@/pages/login-page/login-page.vue"),
   },
   {
     path: "/Company",
-    name: "Company",
+    name: "CompanyPage",
     component: () => import("@/pages/company-page/company-page.vue"),
   },
   {
     path: "/MyPage",
-    name: "MyPage",
+    name: "MyPagePage",
     component: () => import("@/pages/my-page/my-page.vue"),
   },
 ];
