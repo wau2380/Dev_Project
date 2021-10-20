681c01a0ebb928641a83e27323e1861b408c42c7
ahn0min <ahn0min95@gmail.com>
2021-10-21 03:50:32 +0900

    style: 불필요한 div tag 제거

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <div id="app"></div>
   <router-view />
 </template>
 
@@ -10,5 +9,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+body {
+  margin: 0;
 }
 </style>
