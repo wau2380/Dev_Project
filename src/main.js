5fb40d18b66fe4a1faf15fbcf3bd1bf294ba06c1
wau2380(yijung park) <79182893+wau2380@users.noreply.github.com>
2022-01-23 22:07:59 +0900

    zz
    zz

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 
 import { router } from "./router/router.js"; // 라우터 추가하고
-
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import App from "./App.vue";
 
 // Create Vue Instance
 const app = createApp(App);
-
+Vue.use(BootstrapVue);
 app.use(router); // 사용 설정 하기
 
 app.mount("#app");
